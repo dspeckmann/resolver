efe5fcb4cbb984aea70ba68514b57e6a51ba4d64
Danny Speckmann <danny.speckmann@live.de>
2017-01-19 18:21:22 +0100

    Existing source code

---
+++ Resolver/DnsPacket.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resolver
+{
+    class DnsPacket
+    {
+        public short Id { get; set; }
+        public bool Qr { get; set; }
+        public int Opcode { get; set; } // TODO: Enum?
+        public bool Aa { get; set; }
+        public bool Tc { get; set; }
+        public bool Rd { get; set; }
+        public bool Ra { get; set; }
+        public int Rcode { get; set; } // TODO: Enum?
+        public short QdCount { get; set; }
+        public short AnCount { get; set; }
+        public short NsCount { get; set; }
+        public short ArCount { get; set; }
+        public List<DnsQuestion> QuestionSection { get; set; }
+        public List<DnsAnswer> AnswerSection { get; set; }
+        public List<DnsAuthority> AuthoritySection { get; set; }
+        public List<DnsAdditional> AdditionalSection { get; set; }
+
+        // TODO: Put header in extra class?
+        // TODO: Constructor
+        // TODO: DNS namespace
+    }
+}
---
+++ Resolver/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 
+             *      Ugly prototype:
+             * 
+             */
+
+            /* 
+            UdpClient udpClient = new UdpClient();
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse("8.8.8.8"), 53);
+
+            byte[] header = new byte[] {
+                123,      // Message ID
+                123,      
+                1,      // QR, OPCODE, AA, TC, RD
+                0,      // RA, RCODE
+                0,      // QDCOUNT
+                1,      
+                0,      // ANCOUNT
+                0,     
+                0,      // NSCOUNT 
+                0,     
+                0,      // ARCOUNT
+                0
+            };
+
+            byte[] qname1length = new byte[] { (byte)ASCIIEncoding.ASCII.GetByteCount("google") };
+            byte[] qname1 = ASCIIEncoding.ASCII.GetBytes("google");
+            byte[] qname2length = new byte[] { (byte)ASCIIEncoding.ASCII.GetByteCount("com") };
+            byte[] qname2 = ASCIIEncoding.ASCII.GetBytes("com");
+            byte[] termination = new byte[] { 0 };
+            byte[] qtype = new byte[] { 0, 1 };
+            byte[] qclass = new byte[] { 0, 1 };
+
+            byte[] packet = new byte[header.Length + qname1length.Length + qname1.Length + qname2length.Length + qname2.Length + termination.Length + qtype.Length + qclass.Length];
+            Array.Copy(header, packet, header.Length);
+            Array.Copy(qname1length, 0, packet, header.Length, qname1length.Length);
+            Array.Copy(qname1, 0, packet, header.Length + qname1length.Length, qname1.Length);
+            Array.Copy(qname2length, 0, packet, header.Length + qname1length.Length + qname1.Length, qname2length.Length);
+            Array.Copy(qname2, 0, packet, header.Length + qname1length.Length + qname1.Length + qname2length.Length, qname2.Length);
+            Array.Copy(termination, 0, packet, header.Length + qname1length.Length + qname1.Length + qname2length.Length + qname2.Length, termination.Length);
+            Array.Copy(qtype, 0, packet, header.Length + qname1length.Length + qname1.Length + qname2length.Length + qname2.Length + termination.Length, qtype.Length);
+            Array.Copy(qclass, 0, packet, header.Length + qname1length.Length + qname1.Length + qname2length.Length + qname2.Length + termination.Length + qtype.Length, qclass.Length);
+
+            Console.WriteLine("Packet created");
+
+            udpClient.Send(packet, packet.Length, endpoint);
+            byte[] response = udpClient.Receive(ref endpoint);
+
+            Console.WriteLine(ASCIIEncoding.ASCII.GetString(response));
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            */
+        }
+    }
+}
